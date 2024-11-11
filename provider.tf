edc987f0adb303a00adc1d5500003fc0d206b07f
Chieme01 <97136790+Chieme01@users.noreply.github.com>
2024-11-11 11:14:07 -0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -16,25 +16,3 @@ terraform {
     }
   }
 }
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  #resource_provider_registrations = "none"
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources  = false
-    }
-    virtual_machine {
-      delete_os_disk_on_deletion              = true
-    }
-    key_vault {
-      purge_soft_delete_on_destroy            = true
-      purge_soft_deleted_keys_on_destroy      = true
-      purge_soft_deleted_secrets_on_destroy   = true
-    }
-  }
-
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-    
-}
