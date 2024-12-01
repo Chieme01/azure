bbd14b6bcb5f91bcb8719bdd7adaed866ece94c0
chieme <kenyonuc@gmail.com>
2024-12-01 04:33:02 -0800

    enabled data cloud config

--- vm.tf
+++ vm.tf
@@ -58,7 +58,7 @@ resource "azurerm_linux_virtual_machine" "masternode" {
   priority        = local.priority
   eviction_policy = local.spot_instance ? "Deallocate" : null
   #max_bid_price  = 0.01557
-  custom_data     = data.cloudinit_config.azure_cloud_config #data.template_cloudinit_config.masterconfig.rendered #data.cloudinit_config.azure_cloud_config
+  custom_data     = data.cloudinit_config.azure_cloud_config.rendered #data.template_cloudinit_config.masterconfig.rendered #data.cloudinit_config.azure_cloud_config
   #user_data       = filebase64("${path.module}/script.sh")
   tags = merge(var.common_tags, var.masternode_tags)
 }
